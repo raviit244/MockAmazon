ea8b13219f1a17fff181dc4b1b66327e34ef8323
raviit244 <raviit.vij@gmail.com>
2026-05-10 16:13:56 -0400

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,8 +76,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       productId: productId,
       quantity: 1
     });
-    }
+    };
 
+    let cartQuantity = 0;
+
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
