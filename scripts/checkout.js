010f8a60894e3722cda56234a90eb35332b857b9
raviit244 <raviit.vij@gmail.com>
2026-05-14 16:17:51 -0400

    learnt error handling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,14 +6,22 @@ import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js'
 
 async function loadPage() {
+  try {
+    // throw 'error1';
 
-  await loadProductsFetch();
+    await loadProductsFetch();
 
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
+    await new Promise((resolve, reject) => {
+      // throw 'error2';
+      loadCart(() => {
+        // reject('error3');
+        resolve();
+      });
     });
-  });
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+    return;
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
