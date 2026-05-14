41794907f792fded26ba8bef88eaee52ce48a760
raviit244 <raviit.vij@gmail.com>
2026-05-14 15:56:16 -0400

    converted from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,23 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
+async function loadPage() {
+
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
 
@@ -18,6 +35,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
